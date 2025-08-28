6bfb9d6d6c4500fe17bc39ef4c73845ae1e682fd
iamazy <iamazy.me@outlook.com>
2025-08-28 23:26:59 +0800

    feat: use maple mono font

--- nxshell/src/app.rs
+++ nxshell/src/app.rs
@@ -220,7 +220,7 @@ impl NxShell {
 
 fn set_font(ctx: &egui::Context) {
     let name = "仓耳舒圆体";
-    let font = include_bytes!("../assets/fonts/仓耳舒圆体W01.ttf");
+    let font = include_bytes!("../assets/fonts/MapleMono-NF-CN-Light.ttf");
     let mut fonts = egui::FontDefinitions::default();
     fonts
         .font_data
